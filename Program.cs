75aec267c9d608f0c749f70205a8f3c9119a9c70
Lukmaan Kamiss <m.lukmaankamiz@gmail.com>
2025-04-03 14:34:18 +0530

    feat: fixed CORS Error Trial 2

--- Program.cs
+++ Program.cs
@@ -59,7 +59,7 @@ if (app.Environment.IsDevelopment())
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
-app.UseCors("AllowReactApp");
+app.UseCors("AllowFrontend");
 app.UseAuthorization();
 app.MapControllers();
 
